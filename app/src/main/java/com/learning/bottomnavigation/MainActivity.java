bb5964f98f2df8c3a2d7001e8b531a8f8dada762
Krishna <mathi3567>
2020-12-03 15:04:40 +0530

    Initial commit

---
+++ app/src/main/java/com/learning/bottomnavigation/MainActivity.java
@@ -0,0 +1,93 @@
+package com.learning.bottomnavigation;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+    Deque<Integer> integerDeque = new ArrayDeque<>(3);
+    boolean flag = true;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        integerDeque.push(R.id.bm_home);
+//        Load home fragment
+        loadFragment(new HomeFragment());
+
+        bottomNavigationView.setSelectedItemId(R.id.bm_home);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        int id = item.getItemId();
+
+                        if (integerDeque.contains(id)) {
+                            if (id == R.id.bm_home) {
+                                if (integerDeque.size() != 1) {
+                                    if (flag) {
+                                        integerDeque.addFirst(R.id.bm_home);
+                                        flag = false;
+                                    }
+                                }
+                            }
+                            integerDeque.remove(id);
+                        }
+                        integerDeque.push(id);
+                        loadFragment(getFragment(item.getItemId()));
+                        return true;
+                    }
+                }
+        );
+    }
+
+    private Fragment getFragment(int itemId) {
+        switch (itemId) {
+            case R.id.bm_dashboard:
+                bottomNavigationView.getMenu().getItem(0).setChecked(true);
+                return new DashboardFragment();
+
+            case R.id.bm_home:
+                bottomNavigationView.getMenu().getItem(1).setChecked(true);
+                return new HomeFragment();
+
+            case R.id.bm_notification:
+                bottomNavigationView.getMenu().getItem(2).setChecked(true);
+                return new NotificationFragment();
+        }
+        bottomNavigationView.getMenu().getItem(1).setChecked(true);
+        return new HomeFragment();
+    }
+
+    private void loadFragment(Fragment fragment) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.fragment,fragment,fragment.getClass().getSimpleName())
+                .commit();
+    }
+
+    @Override
+    public void onBackPressed() {
+        integerDeque.pop();
+        if (!integerDeque.isEmpty()) {
+            loadFragment(getFragment(integerDeque.peek()));
+        } else {
+            finish();
+        }
+    }
+}
